20b3955853080d57895348328a2523f6420ac687
Hilel Eitan <r.hileleitan@gmail.com>
2026-06-11 12:40:31 -0300

    Refatoração do arquivo teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,13 @@
 print ('Olá, amigo')
-print ('Uma linha acrescentada...')
+print ('')
+
+minha_lista = []
+
+while True:
+    texto = input('Digite qualquer coisa: ')
+
+    if texto != '':
+        minha_lista.append (texto)
+        print (f'Sua lista tem {minha_lista} itens inseridos.')
+    else:
+        print ('Você deve digitar alguma coisa.')
